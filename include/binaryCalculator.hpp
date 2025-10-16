8c8640f0ab6c50a26201fd44ea1a3832450344bd
Kylie <miss.kyliegilbert@gmail.com>
2025-10-16 10:27:17 -0400

    tiny update

--- include/binaryCalculator.hpp
+++ include/binaryCalculator.hpp
@@ -8,7 +8,7 @@
 void displayBinaryMenu(); // V2
 void handleBinaryCalculations(); // V2
 bool validateAndProcessOperation(char& operation);
-std::string padBinary(std::string binary, size_t length);
+std::string padBinary(std::string binary, size_t length); // moving to util files
 std::string trimLeadingZeros(std::string binary);
 std::string addBinary(std::string a, std::string b);
 std::string subtractBinary(std::string a, std::string b);
@@ -16,4 +16,3 @@ std::string addMultipleBinaries(std::vector<std::string>& binaries);
 std::string subtractMultipleBinaries(std::vector<std::string>& binaries);
 
 #endif // BINARY_CALCULATOR_HPP
-
